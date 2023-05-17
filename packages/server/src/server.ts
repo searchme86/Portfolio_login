364c350a1013733e442635e19940b40d541a59d9
searchme86 <searchme86@gmail.com>
2023-05-17 17:48:28 +0900

    노드 웹팩서버 에러플래그 해결 및 nodemone 적용

--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -1,4 +1,7 @@
 import express from 'express';
+import { Request, Response } from 'express';
+import { createProxyMiddleware } from 'http-proxy-middleware';
+
 const app = express();
 const port = 3001;
 
@@ -6,7 +9,13 @@ export interface QueryPayload {
   foo: string;
 }
 
-app.get('/data', (req, res) => {
+const proxyOptions = {
+  target: 'http://localhost:3001',
+  changeOrigin: true,
+};
+const customProxy = createProxyMiddleware(proxyOptions);
+
+app.get('/data', customProxy, (req: Request, res: Response) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   const data: QueryPayload = { foo: 'bar' };
   res.json(data);
